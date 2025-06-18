c3505f68799ba484aac7ffcf2349cdbec2730615
sweet.dh <dlsrksrhk@gmail.com>
2025-06-18 16:48:23 +0900

    변수명 변경

--- client/src/components/MessageList.tsx
+++ client/src/components/MessageList.tsx
@@ -10,25 +10,25 @@ function MessageList({
   messages: ChatMessage[];
   userInfo: SessionUserInfo;
 }) {
-  const messageListRef = useRef<HTMLDivElement | null>(null);
-  const messageEndRef = useRef<HTMLDivElement | null>(null);
+  const scrollTopRef = useRef<HTMLDivElement | null>(null);
+  const scrollBottomRef = useRef<HTMLDivElement | null>(null);
 
-  const isLookingLastMessage = () => {
-    const el = messageListRef.current;
+  const isScrollAtBottom = () => {
+    const el = scrollTopRef.current;
     if (!el) return false;
     return el.scrollTop + el.clientHeight >= el.scrollHeight * 0.9;
   };
 
   useEffect(() => {
-    if (isLookingLastMessage()) {
-      messageEndRef.current?.scrollIntoView({ behavior: "smooth" });
+    if (isScrollAtBottom()) {
+      scrollBottomRef.current?.scrollIntoView({ behavior: "smooth" });
     }
   }, [messages]);
 
   return (
     <div
       className="w-full overflow-y-scroll flex-1 flex flex-col p-4 gap-3"
-      ref={messageListRef}
+      ref={scrollTopRef}
     >
       {messages?.map((message, index) => (
         <Message
@@ -37,7 +37,7 @@ function MessageList({
           type={message.sender === userInfo.username ? "send" : "receive"}
         />
       ))}
-      <div ref={messageEndRef} />
+      <div ref={scrollBottomRef} />
     </div>
   );
 }
